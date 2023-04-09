37dadb8b057aefbdf3eee371249870a3b1530379
muhammadh03 <44944949+muhammadh03@users.noreply.github.com>
2023-04-09 11:13:33 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function foo(){
+    let first = document.getElementById('firstname').value;
+    let middle = document.getElementById('middlename').value;
+    let last = document.getElementById('lastname').value;
+    let mail = document.getElementById('mail').value;
+    console.log(first)
+    console.log(middle)
+    console.log(last)
+    console.log(mail)
+}
